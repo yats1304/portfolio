844731faffd988ad61ed6652e9e09af98ae5aff3
yats1304 <chaubaly@gmail.com>
2025-08-26 21:55:58 +0530

    nav bar for pc

---
+++ components/Home/Navbar/Nav.tsx
@@ -0,0 +1,40 @@
+import { NavLinks } from '@/constant/constant';
+import Link from 'next/link';
+import React from 'react';
+import { BiDownload } from 'react-icons/bi';
+import { FaCode } from 'react-icons/fa';
+
+const Nav = () => {
+  return (
+    <div className='transition-all duration-200 h-[12vh] z-[10000] fixed w-full'>
+        <div className='flex items-center h-full justify-between w-[90%] mx-auto'>
+            {/*LOGO*/}
+            <div className='flex items-center space-x-2'>
+                <div className='w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col'>
+                    <FaCode className='w-5 h-5 text-black'/>
+                </div>
+                <h1 className='text-xl hidden sm:block md:text-2xl text-white font-bold'>YATISH</h1>
+            </div>
+            {/* NavLinks */}
+            <div className='hidden lg:flex items-center space-x-10'>
+                {NavLinks.map((link)=>{
+                    return <Link key={link.id} href={link.url} className='text-base hover:text-cyan-300 text-white
+                    font-medium transition-all duration-200'>
+                        <p>{link.label}</p>
+                    </Link>
+                })}
+            </div>
+            {/* Button */}
+            <div className='flex items-center space-x-4'></div>
+                {/* Resume Button */}   
+                <button className='px-8 py-3.5 text-sm cursor-pointer rounded-lg bg-blue-800 hover:bg-blue-900
+                transition-all duration-300 text-white flex items-center space-x-2'>
+                    <BiDownload className='w-5 h-5'/>
+                    <span>Download Resume</span>
+                </button>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
---
+++ constant/constant.ts
@@ -0,0 +1,37 @@
+export const NavLinks = [
+    {
+        id: 1,
+        url: "#",
+        label: "Home"
+    },
+    {
+        id: 2,
+        url: "#",
+        label: "Services"
+    },
+    {
+        id: 3,
+        url: "#",
+        label: "Resume"
+    },
+    {
+        id: 4,
+        url: "#",
+        label: "Works"
+    },
+    {
+        id: 5,
+        url: "#",
+        label: "Skills"
+    },
+    {
+        id: 6,
+        url: "#",
+        label: "Testimonials"
+    },
+    {
+        id: 7,
+        url: "#",
+        label: "Contact"
+    },
+]
