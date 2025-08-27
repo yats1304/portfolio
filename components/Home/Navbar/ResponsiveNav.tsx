6076bf7cfda2b4947e4876095e09fda0ae540546
yats1304 <chaubaly@gmail.com>
2025-08-27 09:46:48 +0530

    navbar completed

--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Nav from './Nav'
 import MobileNav from './MobileNav'
 
 const ResponsiveNav = () => {
+
+  const [showNav, setShowNav] = useState(false);
+
+  const openNavHandler =()=>setShowNav(true);
+  const closeNavHandler =()=>setShowNav(false);
+
   return (
     <div>
-        <Nav/>
-        <MobileNav/>
+        <Nav openNav={openNavHandler}/>
+        <MobileNav showNav={showNav} closeNav={closeNavHandler}/>
     </div>
   )
 }
