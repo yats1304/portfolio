5d0da9dabc3575ac115d9d55e0d5a20980b92b86
yats1304 <chaubaly@gmail.com>
2025-08-27 16:03:50 +0530

    Services section is done

---
+++ components/Home/Services/ServiceCard.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import React from 'react'
+
+type Props = {
+    icon: string;
+    name: string;
+    description: string;
+}
+
+const ServiceCard = ({ icon, name, description}: Props) => {
+  return (
+    <div>
+        <Image src={icon} alt='img' width={60} height={60}/>
+        <h1 className='mt-6 text-xl md:text-2xl font-bold text-gray-200'>
+            {name}
+        </h1>
+        <p className='mt-6 text-gray-300'>{description}</p>
+    </div>
+  )
+}
+
+export default ServiceCard
---
+++ components/Home/Services/Services.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import ServiceCard from './ServiceCard'
+
+const Services = () => {
+  return (
+    <div className='pt-16 pb-16'>
+        <h1 className='text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white'>
+            Collebrate with brand <br/> and agencies to create <br/> impactful results
+        </h1>
+        <div className='w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 m-20
+        justify-items-center'>
+            <div>
+                <ServiceCard
+                icon="/images/s1.png"
+                name="UI and UX"
+                description="Designing seamless and engaging user experiences using modern web technologies and best UX practices."
+                />
+            </div>
+             <div>
+                <ServiceCard
+                icon="/images/s2.png"
+                name="Web and Mobile App"
+                description="Building fast, responsive, and scalable web interfaces for desktop and mobile using React and the latest frontend frameworks."
+                />
+            </div>
+             <div>
+                <ServiceCard
+                icon="/images/s3.png"
+                name="Design & Creative"
+                description="Transforming ideas into visually appealing and user-friendly digital products through thoughtful design and interactive creativity."
+                />
+            </div>
+             <div>
+                <ServiceCard
+                icon="/images/s4.png"
+                name="Development"
+                description="Developing robust and maintainable web solutions with clean code, performance optimization, and efficient deployment workflows."
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Services
